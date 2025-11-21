f8dbd2ac9527d036b713c89bc61891c73223c23a
Anis <anisbahmani@hotmail.fr>
2025-11-21 12:27:57 +0100

    fix ssl and try catch error

--- consumer/src/main/java/com/example/jms/ConsumerApp.java
+++ consumer/src/main/java/com/example/jms/ConsumerApp.java
@@ -6,14 +6,20 @@ import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 public class ConsumerApp {
     public static void main(String[] args) {
         String brokerURL = "tcp://localhost:61616";
-        try (ConnectionFactory cf = new ActiveMQConnectionFactory(brokerURL);
-             JMSContext context = cf.createContext(Session.AUTO_ACKNOWLEDGE)) {
+        String user = "admin"; // identifiant du docker-compose
+        String pass = "admin";
+
+        try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(brokerURL);
+             JMSContext context = cf.createContext(user, pass, JMSContext.AUTO_ACKNOWLEDGE)) {
 
             Queue queue = context.createQueue("demoQueue");
             JMSConsumer consumer = context.createConsumer(queue);
+
             System.out.println("🟡 En attente de messages...");
             String msg = consumer.receiveBody(String.class);
             System.out.println("📩 Message reçu : " + msg);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- producer/src/main/java/com/example/jms/ProducerApp.java
+++ producer/src/main/java/com/example/jms/ProducerApp.java
@@ -6,12 +6,17 @@ import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 public class ProducerApp {
     public static void main(String[] args) {
         String brokerURL = "tcp://localhost:61616";
-        try (ConnectionFactory cf = new ActiveMQConnectionFactory(brokerURL);
-             JMSContext context = cf.createContext()) {
+        String user = "admin";
+        String pass = "admin";
+
+        try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(brokerURL);
+             JMSContext context = cf.createContext(user, pass, JMSContext.AUTO_ACKNOWLEDGE)) {
 
             Queue queue = context.createQueue("demoQueue");
-            context.createProducer().send(queue, "Hello JMS from Producer!");
+            context.createProducer().send(queue, "Hello JMS with authentication!");
             System.out.println("✅ Message envoyé sur demoQueue !");
+        } catch (JMSRuntimeException e) {
+            e.printStackTrace();
         }
     }
 }
