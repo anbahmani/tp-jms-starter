d26c00acb3e5fdb46c438fa207ceee7f05034342
Anis <anisbahmani@hotmail.fr>
2025-11-24 12:14:48 +0100

    Exercice 3: DLQ and errors

--- consumer/src/main/java/com/example/jms/ConsumerApp.java
+++ consumer/src/main/java/com/example/jms/ConsumerApp.java
@@ -10,21 +10,26 @@ public class ConsumerApp {
         String pass = "admin";
 
         try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(brokerURL);
-             JMSContext context = cf.createContext(user, pass, JMSContext.CLIENT_ACKNOWLEDGE)) {
+             JMSContext context = cf.createContext(user, pass, JMSContext.AUTO_ACKNOWLEDGE)) {
 
-            Queue queue = context.createQueue("transactionQueue");
+            Queue queue = context.createQueue("errorQueue");
             JMSConsumer consumer = context.createConsumer(queue);
 
-            System.out.println("🟡 Lecture manuelle avec ACK...");
+            System.out.println("⚠️ Simulation d'erreurs pour la DLQ...");
             while (true) {
-                Message msg = consumer.receive(3000);
+                String msg = consumer.receiveBody(String.class, 3000);
                 if (msg == null) break;
-                System.out.println("📩 Reçu : " + msg.getBody(String.class));
-                msg.acknowledge();
-                System.out.println("👍 Message acquitté manuellement");
+
+                System.out.println("📩 Reçu : " + msg);
+                if (msg.contains("ERROR")) {
+                    System.out.println("💥 Erreur simulée, message non traité !");
+                    throw new RuntimeException("Erreur de traitement simulée");
+                } else {
+                    System.out.println("✅ Message traité avec succès");
+                }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println("🚨 Exception capturée : " + e.getMessage());
         }
     }
 }
